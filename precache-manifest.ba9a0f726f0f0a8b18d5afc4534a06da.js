c74703bcc46e37b40aa4988d540a509c983e3199
Gabriel <gncorreia@protonmail.com>
2022-05-03 15:38:14 -0300

    Updates

---
+++ precache-manifest.ba9a0f726f0f0a8b18d5afc4534a06da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef2d5753f37040356a53fd8b742a1291",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "16514d4447140f34bc36",
+    "url": "/trivia/static/css/main.fa5ca621.chunk.css"
+  },
+  {
+    "revision": "63378f2469f15fb4c5d1",
+    "url": "/trivia/static/js/2.1c664cdb.chunk.js"
+  },
+  {
+    "revision": "5d1b8067085602e259fd0862f57df793",
+    "url": "/trivia/static/js/2.1c664cdb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16514d4447140f34bc36",
+    "url": "/trivia/static/js/main.4443db89.chunk.js"
+  },
+  {
+    "revision": "23dbb679c736924e4b1a",
+    "url": "/trivia/static/js/runtime-main.6e763e45.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trivia/static/media/trivia.466d153e.png"
+  }
+]);
